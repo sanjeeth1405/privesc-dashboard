847519fbaa741571a9bd6733b1118038a3dbe435
sanjeeth1405 <sanjeeth1405@gmail.com>
2026-02-24 16:56:01 +0530

    Update next.config.js

--- vercel/next.config.js
+++ vercel/next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
-const path = require('path')
-
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname)
-    return config
+  experimental: {
+    outputFileTracingRoot: undefined,
   },
 }
 
